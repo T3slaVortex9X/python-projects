5543c2c33aca871f4d584d5737c0a0f1622b3b52
T3slaVortex9X <laiqh283@gmail.com>
2026-01-10 14:59:08 -0500

    update

--- sum_score.py
+++ sum_score.py
@@ -1,22 +1,31 @@
+
+
 def total(numbers):
     t = 0
     for x in numbers:
         t += x
     return t
 # Taking the numbers of subject
-num_subject = int(input("How many subject do you have?: "))
-# an empty list to get the scores in it
+num_subject = int(input("How many subject do u have?"))
 sum_score = []
 
 for i in range(num_subject):
-    print("Enter the score of subject: ")
-    grades = float(input(f"Subject {i+1}"))
-    sum_score.append(grades)
-total_score = total(sum_score)
+    print("Enter the score of subject:")
+    grade = float(input(f"Subject {i+1}"))
+    sum_score.append(grade)
+    total_score = total(sum_score)
+    average = total_score / num_subject
 
-average = total_score / num_subject
+    print("\n Results")
+    print(f"Your scores: {num_subject}")
+    print(f"Your sum score is : {sum_score}")
+    print(f"average: {average:.2f}")
 
-print("Results")
-print(f"Your scores: {num_subject}")
-print(f"Your sum score: {sum_score}")
-print(f"average: {average}")
+    if average >= 17:
+        print("status; Perfect")
+    elif average >= 14:
+        print("status: good")
+    elif average >= 10:
+        print("statu: passed")
+    else:
+        print("status:Failed")
