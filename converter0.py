77230b76290e295d41712558bd50abfe3b502f69
T3slaVortex9X <laiqh283@gmail.com>
2026-01-12 00:52:21 -0500

    Update converter0.py

--- converter0.py
+++ converter0.py
@@ -4,7 +4,7 @@ Converter = {
     'c_to_f': lambda c: (c * 9/5) + 32,
     'f_to_c': lambda f: (f - 32) * 5/9
 }
-#print(f" {Converter['c_to_f'](25):.1f}f")
+
 print("\n---Wellcome to my project---")
 print("1.Change c to f")
 print("2.Change f to c")
