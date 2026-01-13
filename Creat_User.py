00d8516fd00c8d6a91907eb1b483219de115a82d
T3slaVortex9X <laiqh283@gmail.com>
2026-01-13 16:15:23 -0500

    update

--- Creat_User.py
+++ Creat_User.py
@@ -1,23 +1,24 @@
+import time
+
 users = []
 
 def creat_user():
-    print("\n --- Creat User ---")
-    name = input("Enter name: ")
-    age = int(input("Enter age: "))
-    country = input("Enter country: ")
-    hobby = input("Enter hobby: ")
+    print("\n---Wellcome---")
+    name = input("Enter your name: ")
+    age = int(input("Enter your age: "))
+    country = input("Enter your country: ")
+    hobby = input("Enter your hobby: ")
     user = {
         "name": name,
         "age": age,
         "extra": {
-            "country": country,
             "hobby": hobby,
+            "country": country
         }
     }
     users.append(user)
-    print("User created Successfully!")
+    print("Users Created successfully!")
     return user
-
 def average_age():
     if not users:
         return 0
@@ -25,49 +26,56 @@ def average_age():
 def count_user():
     return len(users)
 def filtered_by_min_age(min_age):
-    return [u for u in users if u["age"] > min_age]
+    return (u for u in users if u["age"] > min_age)
 def show_user(user):
-    print("\n --- User Infor ---")
-    print(f"Name : {user['name']}")
+    print("\n---Users---")
+    print(f"Name: {user['name']}")
     print(f"Age: {user['age']}")
     print("Extra:")
     for k, v in user['extra'].items():
         print(f" {k}: {v}")
-def show_all_users():
-    print("\n --- All User ---")
+def show_all_user():
     if not users:
-        print("No users available")
+        print("No users")
         return
     for u in users:
         show_user(u)
-
 def main():
     while True:
-        print("==== MENU ====")
-        print("1. Create User")
-        print("2 Show All users")
-        print("3. show stats")
-        print("4.filtered by minimum age")
-        print(("5. Exit"))
+        print("\n--Wellcome to Projects--")
+        print("1.Creating user")
+        print("2.Show all users")
+        print("3.Show stats")
+        print("4.filtered by min age")
+        print("5.Exit")
 
-        choice = input("Select an option: ")
-        if choice == "1":
+        choice = int(input("select an option: "))
+        if choice not in [1, 2, 3, 4, 5]:
+            print("Select existing option")
+        elif choice == 1:
             creat_user()
-        elif choice == "2":
-            show_all_users()
-        elif choice == "3":
-            print("\n---Stats---")
-            print(f"Total Users: {count_user()}")
-            print(f"Average Age: {average_age()}")
-        elif choice == "4":
-            age = int(input("Minimum age: "))
+        elif choice == 2:
+            show_all_user()
+        elif choice == 3:
+            print("--STATS")
+            print(f"Total user: {count_user()}")
+            print(f"Average age: {average_age()}")
+        elif choice == 4:
+            age = int(input("enter the minimum age"))
             result = filtered_by_min_age(age)
-            print("\n ---Result---")
+            print("--Result--")
             for u in result:
                 show_user(u)
-        elif choice == "5":
-            print("Good bye.")
-            break
+        elif choice == 5:
+            print("Good bye!")
+            time.sleep(2)
+            print("...")
+            time.sleep(2)
+            print("...........")
+            time.sleep(1)
+            print(".......................")
+            time.sleep(1)
+            print("..................................")
         else:
             print("Invalid option")
 if __name__ == "__main__":
