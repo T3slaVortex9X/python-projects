5beaed3e1b58a1b621621bb16924506275718d0a
T3slaVortex9X <laiqh283@gmail.com>
2026-01-13 09:42:46 -0500

    Update

---
+++ pw_checker.py
@@ -0,0 +1,42 @@
+from random import choice
+
+
+def is_weak_pw(password):
+    score = 0
+    if len(password) >= 8: score += 1
+    elif any(c.isupper() for c in password): score += 1
+    elif any(c.islower() for c in password): score += 1
+    elif any(c.isdigit() for c in password): score += 1
+    elif any(c in "!@#$%^&*?>" for c in password): score += 1
+    return score
+## This is the get strength function
+def get_strength(score):
+    if score <= 2: return "weak"
+    elif score == 3: return "medium"
+    elif score == 4: return "strong"
+    else:
+        return "very strong"
+    ## Yeah the main function that control all function
+def main():
+    while True:
+        print("\n" + "="*30)
+        print("password strenght analyzer")
+        print("1. Analyzer password")
+        print("2. Exit")
+
+        choice = input("select one of the above option")
+
+        if choice == '1':
+            analyze()
+        elif choice == '2':
+            break
+
+while True:
+    def analyze():
+        print("---Password checker---")
+    pw = input("Enter password: ")
+
+    if is_weak_pw(pw):
+        print("weak password")
+    else:
+        print("strong password")
